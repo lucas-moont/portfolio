48b6f001a56152bf92361ead961ec10d9f82a18a
Lucas Monteiro <66444506+lucas-moont@users.noreply.github.com>
2025-03-31 04:14:29 -0300

    --feat Adding finishing back-end project style and animation.

---
+++ assets/typescript/gsap/back_end_projects.ts
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+  gsap.registerPlugin(ScrollTrigger);
+
+  function appearsFromLeft(element: HTMLElement){
+    gsap.fromTo(
+      element,
+      {
+        x: `-120%`,
+      },
+      {
+        x: "0%",
+        opacity: 1,
+        duration: 0.8,
+        ease: "power1",
+        scrollTrigger: {
+          trigger: element,
+          start: "top center",
+          end: "top center",
+        },
+      }
+    );
+  }
+
+  const backEndProjects = document.querySelectorAll(".backEndProjects");
+
+  if(backEndProjects.length > 0) {
+    backEndProjects.forEach(backEndProject => {
+      appearsFromLeft(backEndProject as HTMLElement);
+    });
+  }
+})
